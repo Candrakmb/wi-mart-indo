8a468b8c08218d9cd1130d1843c07d08d50e69cd
candrakmb <candrakmb07@gmail.com>
2024-03-27 22:58:20 +0700

    test midtrans

--- app/Http/Controllers/midtrans/MidtransController.php
+++ app/Http/Controllers/midtrans/MidtransController.php
@@ -13,54 +13,54 @@ class MidtransController extends Controller
 {
     public function receive( Request $request)
     {
-        $serverKey = config('midtrans.server_key');
-        $hashed = hash("sha512",$request->order_id.$request->status_code.$request->gross_amont.$serverKey);
-        if($hashed == $request->signature_key){
-            if($request->transaction_status == 'capture'){
-                $order =Order::where('invoice_number', $request->order_id);
-                $order->update(['status' => '1']);
-            }
-        }
-        
-        // $callback = new CallbackService;
-        
-        // if ($callback->isSignatureKeyVerified()) {
-        //     $notification = $callback->getNotification();
-        //     $order = $callback->getOrder();
-
-        //     // $orderNumber = $order->order_id;
-        //     // $id_order = explode("_",$orderNumber)[1];
- 
-        //     if ($callback->isSuccess()) {
-        //         Order::where('invoice_number', $order->order_id)->update([
-        //             'status' => 1,
-        //             'paid_at'=> Carbon::now(),  
-        //         ]);
+        // $serverKey = config('midtrans.server_key');
+        // $hashed = hash("sha512",$request->order_id.$request->status_code.$request->gross_amont.$serverKey);
+        // if($hashed == $request->signature_key){
+        //     if($request->transaction_status == 'capture'){
+        //         $order =Order::where('invoice_number', $request->order_id);
+        //         $order->update(['status' => '1']);
         //     }
- 
-        //     if ($callback->isExpire()) {
-        //         Order::where('invoice_number', $order->order_id)->update([
-        //             'status' => 5,
-        //         ]);
-        //     }
- 
-        //     if ($callback->isCancelled()) {
-        //         Order::where('invoice_number', $order->order_id)->update([
-        //             'status' => 4,
-        //         ]);
-        //     }
- 
-        //     return response()
-        //         ->json([
-        //             'success' => true,
-        //             'message' => $order->order_id,
-        //         ]);
-        // } else {
-        //     return response()
-        //         ->json([
-        //             'error' => true,
-        //             'message' => 'Signature key torder_idak terverifikasi',
-        //         ], 403);
         // }
+        
+        $callback = new CallbackService;
+        
+        if ($callback->isSignatureKeyVerified()) {
+            $notification = $callback->getNotification();
+            $order = $callback->getOrder();
+
+            // $orderNumber = $order->order_id;
+            // $id_order = explode("_",$orderNumber)[1];
+ 
+            if ($callback->isSuccess()) {
+                Order::where('invoice_number', $order->order_id)->update([
+                    'status' => 1,
+                    'paid_at'=> Carbon::now(),  
+                ]);
+            }
+ 
+            if ($callback->isExpire()) {
+                Order::where('invoice_number', $order->order_id)->update([
+                    'status' => 5,
+                ]);
+            }
+ 
+            if ($callback->isCancelled()) {
+                Order::where('invoice_number', $order->order_id)->update([
+                    'status' => 4,
+                ]);
+            }
+ 
+            return response()
+                ->json([
+                    'success' => true,
+                    'message' => $order->order_id,
+                ]);
+        } else {
+            return response()
+                ->json([
+                    'error' => true,
+                    'message' => 'Signature key torder_idak terverifikasi',
+                ], 403);
+        }
     }
 }
