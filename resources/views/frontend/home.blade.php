9d804350f7994f854485526c0d3ab21b0cce5a0a
Dimas Dharu Ramadhan <dimasddr7@gmail.com>
2024-03-29 15:43:09 +0700

    kategori update tampilan

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 p-0">
-                    <div class="categories__item categories__large__item set-bg"
+                    <div class="categories__large__item set-bg"
                         data-setbg="{{ asset('img') }}/hero.png">
                         <div class="categories__text">
                             <p>Belanja Hemat Hanya Di WIMART</p>
@@ -34,11 +34,7 @@
                 @foreach ($data['category'] as $category)
                     <div class="col-lg-4 col-md-6 kategori">
                         <div class="categories__item set-bg" data-setbg="{{ asset('storage/image/kategori/' . $category->thumbnails) }}">
-                            <div class="categories__card">
-                                <h4>{{ $category->name }}</h4>
-                                <p>{{ $category->Products()->count() }} Item</p>
-                                <a href="{{ route('category.show',$category->slug) }}">Jelajahi</a>
-                            </div>
+                            <a href="{{ route('category.show',$category->slug) }}">{{ $category->name }}</a>
                         </div>
                     </div>
                 @endforeach
